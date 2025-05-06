1acb04929d4fc914939fdafdff50155d7c276b4c
ismael henrique <ismaelhenrique756@gmail.com>
2025-05-06 15:52:21 -0300

    comesso da atividede

---
+++ atvUm.ts
@@ -0,0 +1,7 @@
+/* (5 pontos)
+1 - Faça um programa que calcule e apresente o valor do volume de uma lata, sabendo que PI
+é um valor constante de 3.14159, o programa deve pedir os valores de RAIO e ALTURA, utilize
+a fórmula VOLUME = PI * RAIO² *ALTURA.
+Nome:Ismael Henrique Cardoso dos Santos 
+*/
+
